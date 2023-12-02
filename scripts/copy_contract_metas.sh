a80c0373bbe2c86ba2000ae4d68ec01d31ce6d2f
Thang X. Vu <thang@coongcrafts.io>
2023-12-02 16:50:58 +0700

    chore: copy metadata files

---
+++ scripts/copy_contract_metas.sh
@@ -0,0 +1,5 @@
+cp ./target/ink/motherspace/motherspace.json ../inspaciness/src/metadata
+cp ./target/ink/space/space.json ../inspaciness/src/metadata
+cp ./target/ink/posts_launcher/posts_launcher.json ../inspaciness/src/metadata
+cp ./target/ink/posts/posts.json ../inspaciness/src/metadata
+echo DONE!

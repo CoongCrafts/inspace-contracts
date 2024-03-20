13f91e5bd536a38ff5da6fab1b055d0ba508b050
Thang X. Vu <thang@coongcrafts.io>
2024-03-21 00:44:50 +0700

    remove trait & helper macro crates

--- shared/traits/src/upgradeable.rs
+++
@@ -1,9 +0,0 @@
-/// Ref: https://use.ink/basics/upgradeable-contracts#properties-1
-#[ink::trait_definition]
-pub trait Upgradeable {
-  #[ink(message)]
-  fn set_code_hash(&mut self, code_hash: ::ink::primitives::Hash);
-
-  #[ink(message)]
-  fn code_hash(&self) -> ::ink::primitives::Hash;
-}
